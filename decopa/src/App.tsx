70071ff5818e4d2091106c462d83593adf9eb74d
Joey-dot909 <ologunado@gmail.com>
2025-11-08 16:31:47 +0000

    Decopa commit

---
+++ decopa/src/App.tsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './index.css'
+
+function App() {
+  return (
+    <div className="flex items-center justify-center min-h-screen bg-pink-100">
+      <h1 className="text-4xl font-bold text-red-600">
+        Tailwind is Working! 🚀
+      </h1>
+    </div>
+  );
+}
+
+export default App;
